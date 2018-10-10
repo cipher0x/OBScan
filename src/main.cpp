a765ecc7bcd096350fcb4426bd7870659ee0ac7a
Eric Taylor <cipher0x@gmail.com>
2018-10-10 07:20:32 -0400

    Working on automated PID Report Detect

--- src/main.cpp
+++ src/main.cpp
@@ -37,23 +37,25 @@ int main(int argc, char *argv[])
     uint8_t bit = 0;
 
 //*********PIDs 0>32***********************************************************
-    for(int i = 0; i <= 0x20; i++)
+    for(int i = 0; i <= 0x60; i++)
     {
         ReadPID = elmobd.getPID(i);
         line<<std::hex<<i<<","<<ReadPID.getRawPidVaule()<<",";
 
-        if(i==0)//gets first reported PIDs 0x01 to 0x20
-        {
-
-            reportedPIDs = ReadPID.getRawPidVaule();
-            reportedPIDs = reportedPIDs.substr(6,12);//get just Data
-            std::remove(reportedPIDs.begin(), reportedPIDs.end(), ' ' );//removes whitespace
-
-        }//if
 
 
         if(i >=0 & i <= 0x20)//first 32 PID report
         {
+
+          if(i==0)//gets first reported PIDs 0x01 to 0x20
+          {
+
+              reportedPIDs = ReadPID.getRawPidVaule();
+              reportedPIDs = reportedPIDs.substr(6,12);//get just Data
+              std::remove(reportedPIDs.begin(), reportedPIDs.end(), ' ' );//removes whitespace
+
+          }//if
+
           nib = reportedPIDs.substr(std::floor((i-1)/4),1);
           if(i==0)//if on supported PIDs
           {
@@ -97,7 +99,139 @@ int main(int argc, char *argv[])
             line <<"Reported";
           }
 
-        }//if
+        }//if0 to 0x19
+
+
+
+        if(i >=0x20 & i <= 0x40)//second 32 PID report
+        {
+
+          if(i==0x20)//gets first reported PIDs 0x01 to 0x20
+          {
+
+              reportedPIDs = ReadPID.getRawPidVaule();
+              reportedPIDs = reportedPIDs.substr(6,12);//get just Data
+              std::remove(reportedPIDs.begin(), reportedPIDs.end(), ' ' );//removes whitespace
+
+          }//if
+
+          nib = reportedPIDs.substr(std::floor(((i-0x20)-1)/4),1);
+          if(i==0x20)//if on supported PIDs
+          {
+            nib="";
+            bit = 0;
+            line <<nib<<",";
+            line<<std::hex<<bit<<",";
+          }
+          else//if not supported PIDs
+          {
+            line <<nib<<",";
+
+            //convert hex string to int
+            ss << std::hex<<nib;
+            ss >> intByte;
+            byte = intByte;
+
+            byte = reverseByte(byte);//reverse bit order
+            byte = byte >> 4;
+
+            //convert to bitset object
+            std::bitset<4> bitSetByte (byte);
+            cout <<bitSetByte<<endl;
+
+
+            //reset stringstream
+            ss.str(std::string());
+            ss.clear();
+
+            //get current PID reported bit
+            bit = bitSetByte[((i-0x20)-1)%4];
+            line<<std::hex<<(int)bit<<",";
+          }
+
+          if(i!= 0x20)
+          {
+            if(!bit & (ReadPID.getRawPidVaule() != "NO DATA"))//if reporting no data when data is available
+            {
+              line <<"Not Reported";
+            }
+            else
+            {
+              line <<"Reported";
+            }
+          }//of not on 0x20
+          else//remove extra , on reporting PID
+          {
+            line.str(line.str().erase(line.str().size()-3));
+          }
+
+        }//if 0x20 to 0x40
+
+
+
+        if(i >=0x40 & i <= 0x60)//third 32 PID report
+        {
+
+          if(i==0x40)//gets first reported PIDs
+          {
+
+              reportedPIDs = ReadPID.getRawPidVaule();
+              reportedPIDs = reportedPIDs.substr(6,12);//get just Data
+              std::remove(reportedPIDs.begin(), reportedPIDs.end(), ' ' );//removes whitespace
+
+          }//if
+
+          nib = reportedPIDs.substr(std::floor(((i-0x40)-1)/4),1);
+          if(i==0x40)//if on supported PIDs
+          {
+            nib="";
+            bit = 0;
+            line <<nib<<",";
+            line<<std::hex<<bit<<",";
+          }
+          else//if not supported PIDs
+          {
+            line <<nib<<",";
+
+            //convert hex string to int
+            ss << std::hex<<nib;
+            ss >> intByte;
+            byte = intByte;
+
+            byte = reverseByte(byte);//reverse bit order
+            byte = byte >> 4;
+
+            //convert to bitset object
+            std::bitset<4> bitSetByte (byte);
+            cout <<bitSetByte<<endl;
+
+
+            //reset stringstream
+            ss.str(std::string());
+            ss.clear();
+
+            //get current PID reported bit
+            bit = bitSetByte[((i-0x40)-1)%4];
+            line<<std::hex<<(int)bit<<",";
+          }
+
+          if(i!= 0x40)
+          {
+            if(!bit & (ReadPID.getRawPidVaule() != "NO DATA"))//if reporting no data when data is available
+            {
+              line <<"Not Reported";
+            }
+            else
+            {
+              line <<"Reported";
+            }
+          }//of not on 0x20
+          else//remove extra , on reporting PID
+          {
+            line.str(line.str().erase(line.str().size()-3));
+          }
+
+        }//if 0x40 to 0x60
 
 
     cout << line.str() << endl;
